9880f75cc6476d432670fbb6506d1f8bbf81bd65
Dai Pinshuang <Pinshuang_dai@163.com>
2019-11-14 13:31:39 +0800

    Updates

---
+++ precache-manifest.9c9eaa809b01d823a34feb685ecb2243.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b011bca7b243a54c0c6a06c98985e3e",
+    "url": "/Dps12345/Todolist/index.html"
+  },
+  {
+    "revision": "67ebf6692c3b56f03de9",
+    "url": "/Dps12345/Todolist/static/css/main.ad8343fb.chunk.css"
+  },
+  {
+    "revision": "ecb96aaba26bfc4bfd81",
+    "url": "/Dps12345/Todolist/static/js/2.f13f772b.chunk.js"
+  },
+  {
+    "revision": "67ebf6692c3b56f03de9",
+    "url": "/Dps12345/Todolist/static/js/main.228982c2.chunk.js"
+  },
+  {
+    "revision": "395b19d619fb7d6e4de4",
+    "url": "/Dps12345/Todolist/static/js/runtime-main.e0fb8b88.js"
+  }
+]);
